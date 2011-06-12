9ca3c56d2a2de0d473b5e93ba7b1606a2eba5ca5
noobs <ttraskback@gmail.com>
2011-06-12 15:14:54 +0300

    Added basic rollback.

--- src/net/CraftRepo/Overwatch/RollbackThread.java
+++ src/net/CraftRepo/Overwatch/RollbackThread.java
@@ -3,6 +3,7 @@ package net.CraftRepo.Overwatch;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
 public class RollbackThread extends Thread 
@@ -18,18 +19,6 @@ public class RollbackThread extends Thread
 	{
 		this.target = target;
 		this.sender = sender;
-	}
-	
-	/**
-	 * @param time the time to rollback
-	 */
-	public void setTime(String time)
-	{
-		this.time = time;
-	}
-	
-	public void run()
-	{
 		Overwatchmain.config.load();
 		try 
 		{
@@ -51,8 +40,44 @@ public class RollbackThread extends Thread
 			e.printStackTrace();
 			this.interrupt();
 		}
-		
+	}
+
+	/**
+	 * @param time the time to rollback
+	 */
+	public void setTime(String time)
+	{
+		this.time = time;
+	}
+
+	public void run()
+	{	
 		// Add Code to rollback the blocks. When done, make sure to result.close() to close the resultset and MySQLConnection.st.close() to close the statements!
-		
+		for(int i=1;i<=100;i++)
+		{
+			try 
+			{
+				result.next();
+				Block temp = sender.getWorld().getBlockAt(result.getInt("x"), result.getInt("y"), result.getInt("z"));
+				temp.setTypeIdAndData(result.getInt("item_id"), result.getByte("data"), false);
+				if(result.isLast())
+					break;
+			} catch (SQLException e) 
+			{
+				sender.sendMessage(Overwatchmain.logPrefix + "Error rolling player " + target + ". See console for details.");
+				Overwatchmain.log.warning(Overwatchmain.logPrefix + " Error accessing the MySQL DB.");
+				e.printStackTrace();
+				this.interrupt();
+			}
+		}
+		try {
+			if(result.isLast())
+			{this.interrupt();}
+		} catch (SQLException e) {
+			sender.sendMessage(Overwatchmain.logPrefix + "Error rolling player " + target + ". See console for details.");
+			Overwatchmain.log.warning(Overwatchmain.logPrefix + " Error accessing the MySQL DB.");
+			e.printStackTrace();
+			this.interrupt();
+		}
 	}
 }
